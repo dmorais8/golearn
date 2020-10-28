8c67d5788a62731108e4bec019617a646f95d2b6
David Morais <davidmorais@info.ufrn.br>
2020-10-28 20:00:46 -0300

    json

---
+++ sistemadetipos/tipointerface/tipointerface.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type curso struct {
+	nome string
+}
+
+func main() {
+	var coisa interface{}
+	fmt.Println(coisa)
+
+	coisa = 3
+	fmt.Println(coisa)
+
+	type dinamico interface{}
+	var coisa2 dinamico = "opa"
+	fmt.Println(coisa2)
+
+	coisa2 = curso{"Golang: explorando a linguagem do google"}
+	fmt.Println(coisa2)
+}
